2421f68164238b6ee974b5081284c67b0781605b
Auntapza <chatdanai.k7563@gmail.com>
2023-12-24 15:16:01 +0700

    .

--- sorcecode/src/App.jsx
+++ sorcecode/src/App.jsx
@@ -6,21 +6,26 @@ import minus from "./assets/images/icon-minus.svg"
 
 function App() {
 
-  const [data, setData] = useState()
   const [active, setShow] = useState(-1)
 
-  useEffect(() => {
-    fetchdata()
-  }, [])
-
-  const fetchdata = () => {
-    fetch("../data.json")
-    .then(res => res.text())
-    .then(text => {
-      let details = JSON.parse(text)
-      setData(details)
-    })
-  }
+  let text = [
+    {
+      title: "What is Frontend Mentor, and how will it help me?",
+      ans: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building."
+    },
+    {
+      title: "Is Frontend Mentor free?",
+      ans: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels."
+    },
+    {
+      title: "Can I use Frontend Mentor projects in my portfolio?",
+      ans: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!"
+    },
+    {
+      title: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+      ans: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members."
+    }
+  ]
 
   const setActive = (index) => {
     setShow(index)
@@ -39,7 +44,7 @@ function App() {
           </h1>
         </div>
         <div className="card-body">
-          {data.map((e, i) => {
+          {text.map((e, i) => {
             return (
               <>
                 <div className="accordion" key={i} onClick={() => setActive(i)}>
@@ -51,7 +56,7 @@ function App() {
                     <p>{e.ans}</p>
                   </div>
                 </div>
-                {data.length - 1 === i ? "" : <hr />}
+                {text.length - 1 === i ? "" : <hr />}
               </>
             )
           })}
